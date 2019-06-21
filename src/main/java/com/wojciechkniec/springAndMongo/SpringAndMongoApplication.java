adf7b73c64ededae5ee1854eb043df3e59939f1f
wojciech-kniec <kniecwojciech@gmail.com>
2019-06-21 22:58:43 +0200

    SpringBoot with MongoDB init commit

---
+++ src/main/java/com/wojciechkniec/springAndMongo/SpringAndMongoApplication.java
@@ -0,0 +1,13 @@
+package com.wojciechkniec.springAndMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wojciechkniec/springAndMongo/SpringAndMongoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wojciechkniec.springAndMongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAndMongoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

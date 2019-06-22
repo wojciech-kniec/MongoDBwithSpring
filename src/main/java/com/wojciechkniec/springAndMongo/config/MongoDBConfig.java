46a59001066af37d8950c22fcbcebefc5b501928
wojciech-kniec <kniecwojciech@gmail.com>
2019-06-22 23:46:01 +0200

    New Users added

--- src/main/java/com/wojciechkniec/springAndMongo/config/MongoDBConfig.java
+++ src/main/java/com/wojciechkniec/springAndMongo/config/MongoDBConfig.java
@@ -18,6 +18,10 @@ public class MongoDBConfig {
             public void run(String... args) throws Exception {
                 usersRepository.save(new Users(1, "John", "Bravo", 5000L));
                 usersRepository.save(new Users(2, "Armando", "Newman", 3000L));
+                usersRepository.save(new Users(3, "Ann", "Smith", 4000L));
+                usersRepository.save(new Users(4, "Layla", "Strong", 2500L));
+                usersRepository.save(new Users(5, "Donald", "Triumf", 35000L));
+                usersRepository.save(new Users(6, "Arnold", "Black", 6500L));
             }
         };
     }
